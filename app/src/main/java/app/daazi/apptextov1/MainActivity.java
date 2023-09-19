fa1686b9949f875f43fda3cf03ad6d147c99ced9
Nunes1982 <ansso06061982@gmail.com>
2023-09-19 18:39:32 -0300

    Initial commit

---
+++ app/src/main/java/app/daazi/apptextov1/MainActivity.java
@@ -0,0 +1,35 @@
+package app.daazi.apptextov1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import app.daazi.apptextov1.api.AppUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Nome da Classe - declara um Objeto
+    TextView txtTitulo;
+    TextView txtDataAtual;
+    TextView txtHoraAtual;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtTitulo = findViewById(R.id.txtTitulo);
+        txtDataAtual = findViewById(R.id.txtDataAtual);
+        txtHoraAtual = findViewById(R.id.txtHoraAtual);
+
+        txtTitulo.setText("Curso Android");
+        txtTitulo.setTextColor(getResources().getColor(R.color.colorTextView));
+
+        txtDataAtual.setText(AppUtil.getDataAtual());
+        txtHoraAtual.setText(AppUtil.getHoraAtual());
+
+
+    }
+}
---
+++ app/src/main/java/app/daazi/apptextov1/api/AppUtil.java
@@ -0,0 +1,50 @@
+package app.daazi.apptextov1.api;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Classe de apoio contendo métodos que podem
+ * ser reutilizados em todo o projeto
+ *
+ * Criada por Anderson Nunes - 09/2023
+ *
+ * Versão v1
+ */
+public class AppUtil {
+    /**
+     *
+     * @return devolve a data atual
+     */
+    public static String getDataAtual(){
+
+       String dataAtual = "00-00-0000";
+
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yy");
+        Date date = new Date();
+
+        dataAtual = dateFormat.format(date);
+
+       return dataAtual;
+    }
+
+    /**
+     *
+     * @return devolve a hora atual
+     */
+    public static String getHoraAtual(){
+
+        String horaAtual = "00:00:00";
+
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        Date data = new Date();
+
+        horaAtual = dateFormat.format(data);
+
+        return horaAtual;
+
+    }
+
+}
