0cd8f2a43d971f4af7f16ecac9c0213d33501e0b
Nunes1982 <ansso06061982@gmail.com>
2023-09-21 11:23:43 -0300

    criando validações para o formulário Cadasro

--- app/src/main/java/app/daaziv2/apptextov2/CadastroUsuarioActivity.java
+++ app/src/main/java/app/daaziv2/apptextov2/CadastroUsuarioActivity.java
@@ -4,29 +4,127 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CadastroUsuarioActivity extends AppCompatActivity {
 
     Button btnCadastrar;
+    EditText editNome;
+    EditText editEmail;
+    EditText editSenhaA;
+    EditText editSenhaB;
+    CheckBox ckTermo;
+
+    boolean isFormularioOK;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuario);
 
-        btnCadastrar = findViewById(R.id.btnCadastrar);
+        initFormulario();
 
         btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent iMenuPrincipal = new Intent(CadastroUsuarioActivity.this, MainActivity.class);
-                startActivity(iMenuPrincipal);
+                isFormularioOK = true;
+
+                if (TextUtils.isEmpty(editNome.getText().toString())){
+                    editNome.setError("*");
+                    editNome.requestFocus();
+                    isFormularioOK = false;
+                }
+
+                if (TextUtils.isEmpty(editEmail.getText().toString())){
+                    editEmail.setError("*");
+                    editEmail.requestFocus();
+                    isFormularioOK = false;
+                }
+
+                if (TextUtils.isEmpty(editSenhaA.getText().toString())){
+                    editSenhaA.setError("*");
+                    editSenhaA.requestFocus();
+                    isFormularioOK = false;
+                }
+
+                if (TextUtils.isEmpty(editSenhaB.getText().toString())){
+                    editSenhaB.setError("*");
+                    editSenhaB.requestFocus();
+                    isFormularioOK = false;
+                }
+
+                if (!ckTermo.isChecked()){
+                    ckTermo.setError("*");
+                    ckTermo.requestFocus();
+                    isFormularioOK = false;
+                }
+
+                if (isFormularioOK) {
+
+                    if (!validarSenha()){
+
+                        editSenhaA.setError("*");
+                        editSenhaB.setError("*");
+                        editSenhaA.requestFocus();
+
+                    }else {
+
+                        Intent iMenuPrincipal = new Intent(CadastroUsuarioActivity.this, MainActivity.class);
+                        startActivity(iMenuPrincipal);
+
+                    }
+
+                }
 
             }
         });
 
+    }
+
+    private void initFormulario() {
+
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+        editNome = findViewById(R.id.editNome);
+        editEmail = findViewById(R.id.editEmail);
+        editSenhaA = findViewById(R.id.editSenhaA);
+        editSenhaB = findViewById(R.id.editSenhaB);
+
+        ckTermo = findViewById(R.id.ckTermo);
+
+        isFormularioOK = false;
+
+    }
+
+    public void validarTermo(View view) {
+
+        if (!ckTermo.isChecked()){
+
+            Toast.makeText(getApplicationContext(),
+                    "É necessário aceitar os termos de uso para continuar o cadastro...",
+                    Toast.LENGTH_LONG).show();
+
+        }
+
+    }
+
+    public boolean validarSenha(){
+
+        boolean retono;
+
+        int senhaA, senhaB;
+
+        senhaA = Integer.parseInt(editSenhaA.getText().toString());
+        senhaB = Integer.parseInt(editSenhaB.getText().toString());
+
+        retono = (senhaA == senhaB);
+
+        return retono;
     }
 }
